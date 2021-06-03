4e8d841fb3d9bc361e87808d84bfa02e9641caf5
ky7527 <ky7527@naver.com>
2021-06-03 20:40:54 +0900

    X보다 작은 수

---
+++ baekjoon/10871.py
@@ -0,0 +1,9 @@
+# X보다 작은 수
+import sys
+
+N, X = map(int, sys.stdin.readline().split())
+A = list(map(int, sys.stdin.readline().split()))
+
+for i in range(N):
+    if A[i] < X:
+        print(A[i], end=" ")
